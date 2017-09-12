4ababd83c46ea09f7ed71e9ae630a7bad13802bc
Paul Carduner <paul@carduner.net>
2017-09-12 11:38:14 -0700

    Add top level package.json so this can be deployed on heroku

---
+++ postinstall.sh
@@ -0,0 +1,9 @@
+set -x
+cd packages/js-interpreter-portal
+yarn install --production=false
+cd ../js-interpreter-tyrant
+yarn install --production=false
+yarn run build
+yarn link
+cd ../js-interpreter-portal
+yarn link @code-dot-org/js-interpreter-tyrant
