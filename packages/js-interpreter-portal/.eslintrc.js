a2f3736e52ae168ed394b1601a1324ac5ceb287e
Paul Carduner <paul@carduner.net>
2017-09-21 10:08:22 -0700

    fix linting

--- packages/js-interpreter-portal/.eslintrc.js
+++ packages/js-interpreter-portal/.eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  plugins: ['react'],
   env: {
+    browser: true,
     node: true,
     es6: true,
   },
